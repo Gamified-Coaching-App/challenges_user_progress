0f8593d848a540d1d626e345e48893837e3bbb8b
mariashapo <mart99maria@gmail.com>
2024-02-16 16:48:25 +0000

    m

--- index.mjs
+++ index.mjs
@@ -13,79 +13,79 @@ export async function handler(event) {
   }
 
   const { user_id: userId, distance_in_meters: distance } = event.detail;
-  const numericDistance = Number(distance);
-  const enrollmentTableName = "challenges_user_enrollment";
+  const tableName = "challenges";
+
+  // Query parameters to find active challenges for the user
+  const queryParams = {
+    TableName: tableName,
+    KeyConditionExpression: "#user_id = :user_id",
+    FilterExpression: "#status = :status",
+    ExpressionAttributeNames: {
+      "#user_id": "user_id",
+      "#status": "status"
+    },
+    ExpressionAttributeValues: {
+      ":user_id": userId,
+      ":status": "active"
+    },
+  };
 
   try {
-    // Query the active challenges for the user
-    const queryParams = {
-      TableName: enrollmentTableName,
-      KeyConditionExpression: "#user_id = :user_id",
-      ExpressionAttributeNames: {
-        "#user_id": "user_id",
-        "#status": "status"
-      },
-      ExpressionAttributeValues: {
-        ":user_id": userId,
-        ":status": "active"
-      },
-    };
-
     const queryResult = await documentClient.query(queryParams).promise();
-    const activeChallenges = queryResult.Items;
+    const challenges = queryResult.Items;
 
-    if (activeChallenges.length === 0) {
+    if (challenges.length === 0) {
       console.log(`No active challenges found for user ${userId}`);
-      return { statusCode: 404, body: JSON.stringify({ message: "No active challenges found for the user." }) };
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: "No active challenges found for the user." }),
+      };
     }
 
-    // Process each active challenge
-    for (let challenge of activeChallenges) {
-      // Update completed_meters and check if it exceeds target_meters
-      const challengeId = challenge.challenge_id; // Assuming challenge_id is numeric since you've changed it to a number
-      const updateParams = {
-        TableName: enrollmentTableName,
-        Key: { "user_id": userId, "challenge_id": challengeId },
-        UpdateExpression: "SET completed_meters = completed_meters + :distance",
-        ConditionExpression: "attribute_exists(challenge_id) AND completed_meters + :distance >= target_meters",
-        ExpressionAttributeValues: {
-          ":distance": numericDistance,
-        },
-        ReturnValues: "ALL_NEW" // Returns all of the attributes of the item after the update
+    const updatePromises = challenges.map(async (challenge) => {
+      const newMCompleted = challenge.completed_meters + distance;
+      let updateExpression = "SET completed_meters = :completed_meters";
+      let expressionAttributeNames = {};
+      const expressionAttributeValues = {
+        ":completed_meters": newMCompleted,
       };
 
-      try {
-        const updateResult = await documentClient.update(updateParams).promise();
-        // If completed_meters is greater than or equal to target_meters, update status to "active"
-        if (updateResult.Attributes.completed_meters >= updateResult.Attributes.target_meters) {
-          const statusUpdateParams = {
-            TableName: enrollmentTableName,
-            Key: { "user_id": userId, "challenge_id": challengeId },
-            UpdateExpression: "SET #status = :newStatus",
-            ExpressionAttributeValues: {
-              ":newStatus": "active",
-            },
-            ExpressionAttributeNames: {
-              "#status": "status"
-            }
-          };
-          await documentClient.update(statusUpdateParams).promise();
-        }
-      } catch (err) {
-        if (err.code === 'ConditionalCheckFailedException') {
-          // This means completed_meters did not exceed target_meters, no update needed
-          console.log(`Challenge ${challengeId} for user ${userId} has not reached the target.`);
-        } else {
-          // Handle other errors
-          throw err;
-        }
+      // Check if the challenge is completed
+      if (newMCompleted >= challenge.target_meters) {
+        updateExpression += ", #status = :newStatus";
+        expressionAttributeValues[":newStatus"] = "completed";
+        expressionAttributeNames = { "#status": "status" }; // Only include if updating status
       }
-    }
 
-    console.log(`Successfully processed challenges for user ${userId}`);
-    return { statusCode: 200, body: JSON.stringify({ message: "Challenges processed successfully." }) };
+      const updateParams = {
+        TableName: tableName,
+        Key: { user_id: userId, challenge_id: challenge.challenge_id },
+        UpdateExpression: updateExpression,
+        ExpressionAttributeValues: expressionAttributeValues,
+      };
+
+      // Only add ExpressionAttributeNames to the params if it's not empty
+      if (Object.keys(expressionAttributeNames).length > 0) {
+        updateParams.ExpressionAttributeNames = expressionAttributeNames;
+      }
+
+      return documentClient.update(updateParams).promise();
+    });
+
+    await Promise.all(updatePromises);
+    console.log(`Successfully updated challenges for user ${userId}`);
+
+    // Success response
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Challenges updated successfully." }),
+    };
   } catch (error) {
-    console.error("Error processing challenges for user:", userId, error);
-    return { statusCode: 500, body: JSON.stringify({ error: "Failed to process challenges due to an internal error." }) };
+    console.error("Error updating challenges for user:", userId, error);
+    // Error response
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to update challenges due to an internal error." }),
+    };
   }
 }
