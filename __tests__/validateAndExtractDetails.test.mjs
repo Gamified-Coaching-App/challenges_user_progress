495529481870dcba998dcab2808f3409d84b716e
mariashapo <mart99maria@gmail.com>
2024-03-04 14:00:12 +0000

    Add a test to check for the timestamp conversion

--- __tests__/validateAndExtractDetails.test.mjs
+++ __tests__/validateAndExtractDetails.test.mjs
@@ -1,14 +1,31 @@
-// Import the function to test
 import validateAndExtractDetails from '../index.mjs';
 
 describe('validateAndExtractDetails', () => {
-  it('should extract details correctly from a valid event', () => {
-    // Define a mock event object
+  it('should correctly convert timestamp from seconds to ISO string format', () => {
+    const timestampInSeconds = 1609459200; // This represents a specific moment, e.g., 2021-01-01T00:00:00Z
+    const expectedISODateTime = "2021-01-01T00:00:00.000Z";
+
     const mockEvent = {
       detail: {
         user_id: '123',
         distance_in_meters: 1000,
-        timestamp_local: Math.floor(new Date().getTime() / 1000), // Current time in seconds
+        timestamp_local: timestampInSeconds,
+        activity_type: 'RUNNING',
+      }
+    };
+
+    const extractedDetails = validateAndExtractDetails(mockEvent);
+
+    expect(extractedDetails.workoutTimeConverted).toBe(expectedISODateTime);
+  });
+
+  it('should extract details correctly from a valid event', () => {
+    // Define your mock event with appropriate data for validation
+    const mockEvent = {
+      detail: {
+        user_id: '123',
+        distance_in_meters: 1000,
+        timestamp_local: 1609459200,
         activity_type: 'RUNNING',
       }
     };
@@ -16,11 +33,10 @@ describe('validateAndExtractDetails', () => {
     // Call the function with the mock event
     const extractedDetails = validateAndExtractDetails(mockEvent);
 
-    // Expectations
-    expect(extractedDetails.userId).toBe(mockEvent.detail.user_id);
-    expect(extractedDetails.distance).toBe(mockEvent.detail.distance_in_meters);
+    // Expectations for other details extracted
+    expect(extractedDetails.userId).toBe('123');
+    expect(extractedDetails.distance).toBe(1000);
+    expect(extractedDetails.activityType).toBe('RUNNING');
     // Add more assertions as needed
   });
-
-  // You can add more test cases here, such as testing for invalid events
 });
